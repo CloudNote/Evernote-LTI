b0c958a95ac9e0ffbb032d8555174545d1d23957
Aleksandr Soborov <asoborov@gmail.com>
2013-02-11 20:25:36 -0600

    Added https://github.com/instructure/lti_tool_provider_example as a starting point

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'ims-lti',   "~>1.0.2"
+
+group :development do
+  gem 'shotgun'
+end
---
+++ tool_provider.rb
@@ -0,0 +1,110 @@
+require 'sinatra'
+require 'ims/lti'
+# must include the oauth proxy object
+require 'oauth/request_proxy/rack_request'
+
+enable :sessions
+set :protection, :except => :frame_options
+
+get '/' do
+  erb :index
+end
+
+# the consumer keys/secrets
+$oauth_creds = {"test" => "secret", "testing" => "supersecret"}
+
+def show_error(message)
+  @message = message
+  erb :error
+end
+
+def authorize!
+  if key = params['oauth_consumer_key']
+    if secret = $oauth_creds[key]
+      @tp = IMS::LTI::ToolProvider.new(key, secret, params)
+    else
+      @tp = IMS::LTI::ToolProvider.new(nil, nil, params)
+      @tp.lti_msg = "Your consumer didn't use a recognized key."
+      @tp.lti_errorlog = "You did it wrong!"
+      return show_error "Consumer key wasn't recognized"
+    end
+  else
+    return show_error "No consumer key"
+  end
+
+  if !@tp.valid_request?(request)
+    return show_error "The OAuth signature was invalid"
+  end
+
+  if Time.now.utc.to_i - @tp.request_oauth_timestamp.to_i > 60*60
+    return show_error "Your request is too old."
+  end
+
+  # this isn't actually checking anything like it should, just want people
+  # implementing real tools to be aware they need to check the nonce
+  if was_nonce_used_in_last_x_minutes?(@tp.request_oauth_nonce, 60)
+    return show_error "Why are you reusing the nonce?"
+  end
+
+  # save the launch parameters for use in later request
+  session['launch_params'] = @tp.to_params
+
+  @username = @tp.username("Dude")
+end
+
+# The url for launching the tool
+# It will verify the OAuth signature
+post '/lti_tool' do
+  authorize!
+
+  if @tp.outcome_service?
+    # It's a launch for grading
+    erb :assessment
+  else
+    # normal tool launch without grade write-back
+    @tp.lti_msg = "Sorry that tool was so boring"
+    erb :boring_tool
+  end
+end
+
+# post the assessment results
+post '/assessment' do
+  if session['launch_params']
+    key = session['launch_params']['oauth_consumer_key']
+  else
+    return show_error "The tool never launched"
+  end
+
+  @tp = IMS::LTI::ToolProvider.new(key, $oauth_creds[key], session['launch_params'])
+
+  if !@tp.outcome_service?
+    return show_error "This tool wasn't lunched as an outcome service"
+  end
+
+  # post the given score to the TC
+  res = @tp.post_replace_result!(params['score'])
+
+  if res.success?
+    @score = params['score']
+    @tp.lti_msg = "Message shown when arriving back at Tool Consumer."
+    erb :assessment_finished
+  else
+    @tp.lti_errormsg = "The Tool Consumer failed to add the score."
+    show_error "Your score was not recorded: #{res.description}"
+  end
+end
+
+get '/tool_config.xml' do
+  host = request.scheme + "://" + request.host_with_port
+  url = host + "/lti_tool"
+  tc = IMS::LTI::ToolConfig.new(:title => "Example Sinatra Tool Provider", :launch_url => url)
+  tc.description = "This example LTI Tool Provider supports LIS Outcome pass-back."
+
+  headers 'Content-Type' => 'text/xml'
+  tc.to_xml(:indent => 2)
+end
+
+def was_nonce_used_in_last_x_minutes?(nonce, minutes=60)
+  # some kind of caching solution or something to keep a short-term memory of used nonces
+  false
+end
